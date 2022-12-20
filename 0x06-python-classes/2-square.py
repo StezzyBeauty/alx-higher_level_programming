0ae538fd2305e7bd27edeee1a5f56e25519e56ad
Stella <stellaugochukwu100@gmail.com>
2022-12-20 16:41:48 +0100

    mandatory python classes task

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
-''' A Module that creates a Square object '''
+"""Defines a class Square"""
 
-class Square;
-''' Creating an Object template '''
-    def __init__(self, size = 0):
-        '''
-            The init method initializes the class instance
 
-        @self:
-            A parameter used to refer to the class instance
+class Square:
+    """
+    A square class with a private object attribute.
+    Check the type of the argument and raises an exception on error
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
 
-        @size:
-            The size of the square, must be a +ve integer
-        '''
-        if type(size) is int:
-            if size < 0:
-                raise ValueError('Size must be >= 0')
-            else:
-                self.__size = size
-        else:
-            raise TypeError('Size must be an integer')
+        self.__size = size
